237b6858728b8cf1428fa11c4a5d75bfbe7d2825
Guleon Seon <guleonseon@163.com>
2020-03-03 14:20:51 +0800

    add .local & .bashrc

---
+++ .local/service.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+###########################################################
+###		This is a services script		###
+###########################################################
+
+LOCAL=$HOME/.local
+
+#frpc service
+frpClient() {
+LOG=$LOCAL/log/frpc.log
+rm $LOG
+PID=`pidof "frpc"`
+if [[ $PID != ""  ]]
+then
+	kill -9 $PID > /dev/null 2>&1
+	sleep 1
+fi
+nohup $LOCAL/bin/frpc -c $LOCAL/etc/frp/frpc.ini > $LOG 2>&1 &
+}
+
+#v2ray server service
+v2rayServer() {
+LOG=$LOCAL/log/v2ray-server.log
+rm $LOG
+PID=`ps aux |grep "v2ray-server"| awk '{print $2}'`
+if [[ $PID != ""  ]]
+then
+	 kill -9 $PID > /dev/null 2>&1
+	sleep 1
+fi
+nohup  $LOCAL/bin/v2ray/v2ray -config $LOCAL/etc/v2ray/v2ray-server.json > $LOG 2>&1 & 
+}
+
+#v2ray client service
+v2rayClient() {
+LOG=$LOCAL/log/v2ray-client.log
+rm $LOG
+PID=`ps aux |grep "v2ray-client"| awk '{print $2}'`
+if [[ $PID != ""  ]]
+then
+	 kill -9 $PID > /dev/null 2>&1
+	sleep 1
+fi
+nohup  $LOCAL/bin/v2ray/v2ray -config $LOCAL/etc/v2ray/v2ray-client.json > $LOG 2>&1 & 
+}
+
+frpClient && v2rayServer
--- install.sh
+++ install.sh
@@ -2,4 +2,28 @@
 
 echo "### Welcome to Google Cloud Shell Auto Deploy Script ###"
 
+USER=`whoami`
+
+if [[ $USER == "root" ]]
+then
+  echo "Plase run as normal user!"
+  exit
+fi
+
 #get .local
+git clone https://github.com/guleonseon/gcs-auto.git
+
+#copy .local .bashrc to $HOME
+cp gcs-auto/.local $HOME
+
+if [[ -f $HOME/.bashrc ]]
+then
+  mv $HOME/.bashrc $HOME/.bashrc_bak
+fi
+
+cp gcs-auto/.bashrc $HOME
+
+source $HOME/.bashrc
+
+echo "Deploy has all done, enjoy it!"
+
